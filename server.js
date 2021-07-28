d125dd5120ae3005f7c0f9a44b75c2d0bc1fb7cb
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-07-28 09:35:05 +0300

    created routes and imported to server.js file

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log('hello form server');
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+const { connect } = require('mongodb');
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const mainRoutes = require('./server/utilities/mainroutes');
+app.use('/', mainRoutes);
+
+const PORT = process.env.PORT;
+
+connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, console.log(`Server online and running on port ${PORT}`));
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.log('Failed to connect to MongoDB', error);
+  }
+};
+
+connectDB();
---
+++ server/utilities/mainRoutes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const router = express.Router();
+
+const User = require('../models/userModel');
+
+router.get('/users', async (req, res) => {
+  try {
+    const result = await User.find();
+    res.json(result);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.post('/users/new', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    let savedUser = newUser.save();
+    res.json({ success: true, newUser: savedUser });
+  } catch (error) {
+    res.status(400).json({ success: false, error });
+  }
+});
+
+router.delete('/users/delete/:id', async (req, res) => {
+  try {
+    const result = await User.findByIdAndDelete(req.params.id);
+    res.json({ deleteSuccessful: true, deletedUser: result });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.put('/users/edit/:id', async (req, res) => {
+  try {
+    result = await User.findByIdAndUpdate(req.params.id, req.body);
+    res.json({ updateSuccessful: true, updatedUser: result });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+module.exports = router;
