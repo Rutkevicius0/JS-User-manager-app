3a64d73627ef661339e1c0ae438c19b508e1dbe2
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-07-28 12:00:14 +0300

    get, post and delete request added

---
+++ client/src/utilities/requests.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/users';
+
+export const getAllUsers = async () => {
+  try {
+    const result = await axios.get(baseUrl);
+    console.log(result.data);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addNewUser = async (newUserDataObj) => {
+  try {
+    const result = await axios.post(`${baseUrl}/new`, newUserDataObj);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteUser = async (userId) => {
+  try {
+    const deleteResult = await axios.delete(`${baseUrl}/delete/${userId}`);
+    return deleteResult.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
