ddb0474bf413bf074e779ddada4fb460ed5e2ede
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-07-28 10:00:48 +0300

    changed app.js to jsx and cleaned app.css

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="app-container">
+        <h1>Vartotojų</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
