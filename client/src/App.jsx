0e017a3eeb888c63a465c8797939d1ff60149a11
Rutkevicius0 <79320802+Rutkevicius0@users.noreply.github.com>
2021-07-28 11:56:17 +0300

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserForm from './components/UserForm';
 
 class App extends Component {
   state = {};
   render() {
     return (
       <div className="app-container">
-        <h1>Vartotojų</h1>
+        <h1>Vartotojų tvarkyklė</h1>
+        <UserForm />
       </div>
     );
   }
